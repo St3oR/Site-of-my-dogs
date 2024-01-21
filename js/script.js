0a766f7e4cff12aa88d36859b791b5dd3b8a9e8a
St3oR <teodor_mihalcea@yahoo.com>
2024-01-22 01:00:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('blackie-link').addEventListener('click', function() {
+    document.getElementById('blackie-pop').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('blackie-pop').style.display = 'none';
+}
+
+
+document.getElementById('xena-link').addEventListener('click', function() {
+    document.getElementById('xena-pop').style.display = 'block';
+});
+
+function closePopups() {
+    document.getElementById('xena-pop').style.display = 'none';
+}
